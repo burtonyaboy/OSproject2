3e8230129df98b1f9d608a6ace5d71e9d1ada72c
root <root@DESKTOP-O29IKUL.localdomain>
2020-04-15 16:55:11 -0500

    need to switch to my mac

--- main.c
+++ main.c
@@ -2,6 +2,15 @@
 
 int main(int argc, const char **argv)
 {
+	if(argc < 5)
+	{
+		printf("Error: go fuck yourself!\n");
+		return 1;
+	}
+
+	FILE *fptr;
+	fptr = fopen(argv[1],"r");
+
 	printf("what r u lookin at\n");
 	return 0;
 }
